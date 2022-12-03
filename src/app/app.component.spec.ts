74af47ef9f126ac7507ccd6bcf7fe820622fef2f
boldyriev <d.boldyriev@comparus.de>
2022-12-03 10:15:14 +0200

    change app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'website'`, () => {
+  it(`should have as title 'varenik'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('website');
+    expect(app.title).toEqual('varenik');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('website app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('varenik app is running!');
   });
 });
